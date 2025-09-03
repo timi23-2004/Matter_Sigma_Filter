5d881511fd6727e868d071a6d2e27a6a0410b7a2
timi23-2004 <timicit2@gmail.com>
2025-09-03 19:17:27 +0300

    Update pr_log.h

--- pr_log.h
+++ pr_log.h
@@ -1,4 +1,3 @@
-#include "pr_pcap.h"
 #ifndef PR_LOG_H
 #define PR_LOG_H
 #include <stdio.h>
@@ -14,3 +13,4 @@ void init_log_file(const char *filename);
 void close_log_file();
 void log_packet(const struct pcap_pkthdr *header,const packet_info_t *pktinfo);
 #endif // PR_LOG_H
+
