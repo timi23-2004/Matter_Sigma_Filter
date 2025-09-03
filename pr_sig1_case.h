800609f584606570dd4fd31fedae209276afb302
timi23-2004 <timicit2@gmail.com>
2025-09-03 19:22:10 +0300

    Update pr_sig1_case.h

--- pr_sig1_case.h
+++ pr_sig1_case.h
@@ -9,7 +9,7 @@ void mac_pair_count_list_init(void);
 void mac_pair_count_list_uninit(void);
 void mac_pair_count_add(const unsigned char *src_mac, const unsigned char *dest_mac);
 void mac_pair_count_print_all(void);
-int is_mac_pair_blocked(const unsigned char *src_mac, const unsigned char *dest_mac);
+uint8_t is_mac_pair_blocked(const unsigned char *src_mac, const unsigned char *dest_mac);
 void mac_pair_count_reset_counts(void);
 
 #endif//PR_CASE_H
