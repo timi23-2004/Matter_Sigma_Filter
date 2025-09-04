cab0527eb6bc24e7017876373dd9adb7a8883b3e
timi23-2004 <timicit2@gmail.com>
2025-09-04 10:09:50 +0300

    Delete pr_log.h

--- pr_log.h
+++
@@ -1,9 +0,0 @@
-#ifndef PR_LOG_H
-#define PR_LOG_H
-#include <stdio.h>
-#include <stdlib.h>
-extern FILE *log_file;
-void init_log_file(const char *filename);
-void close_log_file();
-void log_packet(const struct pcap_pkthdr *header,const packet_info_t *pktinfo);
-#endif // PR_LOG_H
